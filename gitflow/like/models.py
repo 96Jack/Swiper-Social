c2edd3ad11412c32e80e0a848c89dbbb7059a37b
96Jack <xzw_246850@163.com>
2022-03-06 09:29:14 +0800

    modified models views

--- gitflow/like/models.py
+++ gitflow/like/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Like(models.Model):
+    uid = models.IntegerFiled()
+    name = models.CharField()
+    other_id = models.IntegerField()
+    other_name = models.CharField()
+
+
+
