8fb9ada7ca90b8ed85b8b7870f637e6b2bbd786c
96Jack <xzw246850@163.com>
2022-03-06 10:11:32 +0800

    modify settings

--- gitflow/gitflow/settings.py
+++ gitflow/gitflow/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9weov101p=zko)j2ln%%r-wsjms1=v780ptlh%)hzry21@hj&4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'like',
 ]
 
 MIDDLEWARE = [
