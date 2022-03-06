6da7b4f3501de6e143a9e8ec640abe877ac50dc2
96Jack <xzw246850@163.com>
2022-03-06 10:26:51 +0800

    add folow

--- gitflow/gitflow/settings.py
+++ gitflow/gitflow/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-9weov101p=zko)j2ln%%r-wsjms1=v780ptlh%)hzry21@hj&4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
--- gitflow/like/models.py
+++ gitflow/like/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Like(models.Model):
-    uid = models.IntegerFiled()
+    uid = models.IntegerField()
     name = models.CharField()
     other_id = models.IntegerField()
     other_name = models.CharField()
