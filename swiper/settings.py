a3a5e628c2a7d28a066e4f4c52f17b8be66d1698
96Jack <xzw246850@163.com>
2022-03-07 12:54:31 +0800

    set up db connect

--- swiper/settings.py
+++ swiper/settings.py
@@ -79,9 +79,14 @@ WSGI_APPLICATION = 'gitflow.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sh1903',
+        'HOST': '120.24.212.17',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': '12345678'
     }
+
 }
 
 
@@ -107,15 +112,16 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-hans'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+# 时区
+USE_TZ = False 
 
 
 # Static files (CSS, JavaScript, Images)
