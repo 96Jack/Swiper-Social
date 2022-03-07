9ef9e7e7e08f9424f28b980a4f7402480477be71
96Jack <xzw246850@163.com>
2022-03-07 14:28:32 +0800

    user db make migrations

--- user/models.py
+++ user/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 # Create your models here.
 
-class Like(models.Model):
+class User(models.Model):
     # 提供可选项，元组，前面一个值存在数据库，后面的值时提示用
     SEX = (
         ('male',   '男性'),
@@ -25,9 +25,9 @@ class Like(models.Model):
     sex           = models.CharField(max_length=8, choices=SEX, verbose_name="性别  ")
     # birth_year    = models.CharField( verbose_name="出⽣年")
     # birth_month   = models.CharField( verbose_name="出⽣⽉    ")
-    birth_day     = models.CharField( default='1990-1-1', verbose_name="出⽣⽇    ")
+    birth_day     = models.CharField(max_length=8, default='1990-1-1', verbose_name="出⽣⽇    ")
     # 形象介绍的内容较多，一般存在服务器，给网址
-    avatar        = models.CharField( max_length='256', verbose_name="个⼈形象   ")
+    avatar        = models.CharField( max_length=256, verbose_name="个⼈形象   ")
     location      = models.CharField( max_length=20, choices=LOCATION, verbose_name="常居地         ")
              
 
