44333b1a6c53fa5d912371c40515edd706011d9e
96Jack <xzw246850@163.com>
2022-03-30 11:10:44 +0800

    用monkey patch 统一增加缓存层

--- common/keys.py
+++ common/keys.py
@@ -3,4 +3,4 @@ VCODE_KEY = 'Vcode-%s'           # 验证码的key
 SUPERLIKED_KEY = 'SUPERLIKED-%s' # 某人被超级喜欢过
 REWIND_KEY = 'REWIND_KEY-%s'     # 反悔次数的 key
 PROFILE_KEY = 'PROFILE_KEY-%s'   # 用户资料
-
+MODEL_KEY = 'model_key-%s-%s'    # 模型的键值
