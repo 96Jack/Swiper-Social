31173588e8de8ee4c805f9ca10b1c6215972a21e
96Jack <xzw246850@163.com>
2022-03-30 14:47:52 +0800

    MonkeyPatch save'

--- libs/orm.py
+++ libs/orm.py
@@ -1,4 +1,6 @@
+from statistics import mode
 from django.db.models import query
+from django.db import models
 
 from libs.cache import  rds
 from common.keys import MODEL_KEY
@@ -11,24 +13,34 @@ def get(self, *args, **kwargs):
         key = MODEL_KEY % (self.model.__name__, pk)
         model_obj = rds.get(key)
         if isinstance(model_obj, self.model):
-            print("从缓存中获取model: %s" % model_obj)
+            # print("从缓存中获取model: %s" % model_obj)
             return model_obj
 
     # 缓存中没有，从数据库中获取
     model_obj = self._get(*args, **kwargs)
-    print("从数据库中获取model: %s" % model_obj)
+    # print("从数据库中获取model: %s" % model_obj)
 
     # 将取出的数据写入缓存
     key =MODEL_KEY % (self.model.__name__, model_obj.pk)
     rds.set(key, model_obj)
-    print("将model写入缓存: %s" % model_obj)
+    # print("将model写入缓存: %s" % model_obj)
     return model_obj
     
-
+def save(self, force_insert=True, force_update=False, using=None, update_fields=None):
+    #使用原来的save() 将model写入数据库
+    self._save(force_insert, force_update, using, update_fields)
+    #将model_obj保存到缓存
+    key = MODEL_KEY % (self.__class__.__name__, self.pk)
+    rds.set(key, self)
+    
 def patch():
+    """通过MonkeyPatch给原ORM添加缓存处理"""
     # 保留原get方法
-    query.QuerySet._get = query.QuerySet.get
-
+    query.QuerySet._get = query.QuerySet.get    
     # 重写get方法
-
     query.QuerySet.get  = get
+
+    models.Model._save = models.Model.save
+    # 重写save方法
+    models.Model.save = save
+
